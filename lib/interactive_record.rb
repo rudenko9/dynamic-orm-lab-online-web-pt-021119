acee137e86acfff3aff8e150a7170f338ef0aa76
Nataliya R <n.rudenko@gmail.com>
2019-05-07 16:59:33 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -47,13 +47,7 @@ def save
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert};")[0][0]
   end
   
-  def self.find_by_name(name)
-    sql = <<-SQL
-    SELECT * FROM #{self.table_name} WHERE name = ?
-    SQL
-    
-    DB[:conn].execute(sql, name)
-  end
+  
   
   
   def self.find_by(attribute)
